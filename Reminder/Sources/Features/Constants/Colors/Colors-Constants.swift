07cd79a7474793db079b6e40aac18e46b43e0b27
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-11 11:54:27 -0300

    Como codar uma View

---
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Fabiano Braga on 11/12/25.
+//
+
+import Foundation
--- Reminder/Sources/Features/Splach/SplashView.swift
+++ Reminder/Sources/Features/Splach/SplashView.swift
@@ -9,6 +9,23 @@ import Foundation
 import UIKit
 
 class SplashView: UIView {
+    private let logoImageView:UIImageView = {
+       let image = UIImageView()
+        image.image = UIImage(named: "Logo")
+        image.contentMode = .scaleAspectFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let titleLogoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Remind"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     init() {
         super.init(frame: .zero)
         setupUI()
@@ -19,11 +36,18 @@ class SplashView: UIView {
     }
     
     private func setupUI() {
-        self.backgroundColor = .white
+        self.backgroundColor = .systemRed
+        addSubview(logoImageView)
+        addSubview(titleLogoLabel)
         setupContrainsts()
     }
     
     private func setupContrainsts() {
-        
+        NSLayoutConstraint.activate([
+            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
+            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8),
+            logoImageView.bottomAnchor.constraint(equalTo: titleLogoLabel.bottomAnchor)
+        ])
     }
  }
